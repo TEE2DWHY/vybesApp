9e06027e331c0e2bf924e99cd2439c1b93d0061c
TEE2DWHY <isaacolorunfemi330@gmail.com>
2024-08-05 00:20:35 +0100

    started working on sign-in page

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import {
   Image,
   SafeAreaView,
@@ -12,6 +12,7 @@ import logoTwo from "../../assets/images/logo2.png";
 import union from "../../assets/images/union.png";
 
 const SignIn = () => {
+  const [section, setSection] = useState(1);
   return (
     <SafeAreaView className="h-full bg-purple-darker">
       <StatusBar backgroundColor="#391753" barStyle="light-content" />
@@ -35,37 +36,44 @@ const SignIn = () => {
           //   resizeMode="contain"
         />
       </View>
-      <View className="bg-white-normal h-full -mt-[50px] pt-5 rounded-tr-[80px]">
-        <Text className="self-center text-2xl font-axiformaBlack mt-3">
-          Let's Know Your Gender
-        </Text>
-        <Text className="self-center text-sm font-axiformaLight mt-2 px-5 text-center">
-          Selecting Your Gender Helps You Find Your Right Type Of Vybe.
-        </Text>
-        <View className="mt-5 px-5">
-          <TouchableOpacity className="border border-blue-300 rounded-full py-4 px-6 mb-4 flex-row items-center justify-between">
-            <Text className="text-purple-darker text-base font-axiformaBlack">
-              Male
+      {section === 1 && (
+        <View className="bg-white-normal h-full -mt-[50px] pt-5 rounded-tr-[80px]">
+          <Text className="self-center text-2xl font-axiformaBlack mt-3">
+            Let's Know Your Gender
+          </Text>
+          <Text className="self-center text-sm font-axiformaLight mt-2 px-5 text-center">
+            Selecting Your Gender Helps You Find Your Right Type Of Vybe.
+          </Text>
+          <View className="mt-5 px-5">
+            <TouchableOpacity className="border border-blue-300 rounded-full py-4 px-6 mb-4 flex-row items-center justify-between">
+              <Text className="text-purple-darker text-base font-axiformaBlack">
+                Male
+              </Text>
+              <Text className="text-purple-darker text-base">♂️</Text>
+            </TouchableOpacity>
+            <TouchableOpacity className="border border-blue-300 rounded-full py-4 px-6 mb-4 flex-row items-center justify-between">
+              <Text className="text-purple-darker text-base font-axiformaBlack">
+                Female
+              </Text>
+              <Text className="text-purple-darker text-lg">♀️</Text>
+            </TouchableOpacity>
+            <TouchableOpacity className="border border-blue-300 rounded-full py-4 px-6 mb-4 flex-row items-center justify-between">
+              <Text className="text-white text-base font-axiformaBlack">
+                Non-Binary
+              </Text>
+              <Text className="text-white text-lg">⚧️</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            className="self-center bg-purple-dark rounded-full py-4 px-20 mt-5 w-[90%]"
+            onPress={() => setSection(2)}
+          >
+            <Text className="text-white-normal text-lg text-center font-axiformaBlack">
+              Next
             </Text>
-            <Text className="text-purple-darker text-base">♂️</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="border border-blue-300 rounded-full py-4 px-6 mb-4 flex-row items-center justify-between">
-            <Text className="text-purple-darker text-base font-axiformaBlack">
-              Female
-            </Text>
-            <Text className="text-purple-darker text-lg">♀️</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="border border-blue-300 rounded-full py-4 px-6 mb-4 flex-row items-center justify-between">
-            <Text className="text-white text-base font-axiformaBlack">
-              Non-Binary
-            </Text>
-            <Text className="text-white text-lg">⚧️</Text>
           </TouchableOpacity>
         </View>
-        <TouchableOpacity className="self-center bg-purple-normal rounded-full py-4 px-20 mt-5">
-          <Text className="text-white-normal text-lg">Next</Text>
-        </TouchableOpacity>
-      </View>
+      )}
     </SafeAreaView>
   );
 };
