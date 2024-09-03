e571670ddbc048d735fbce3afbcca3bc84393cab
TEE2DWHY <isaacolorunfemi330@gmail.com>
2024-09-03 15:37:37 +0100

    updated the landing page for vybes tab

--- app/(tabs)/vybes/index.jsx
+++ app/(tabs)/vybes/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -7,9 +7,16 @@ import {
   ScrollView,
   SafeAreaView,
 } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Feather from "@expo/vector-icons/Feather";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
+import { LinearGradient } from "expo-linear-gradient";
 
 const App = () => {
-  const storiesData = [
+  const [activeSection, setActiveSection] = useState("Baddies");
+
+  const baddiesData = [
     {
       id: 1,
       name: "Olayemi",
@@ -37,76 +44,199 @@ const App = () => {
     },
   ];
 
+  const vybersData = [
+    {
+      id: 1,
+      name: "Michael",
+      imageUrl: "https://randomuser.me/api/portraits/men/12.jpg",
+    },
+    {
+      id: 2,
+      name: "Sarah",
+      imageUrl: "https://randomuser.me/api/portraits/women/15.jpg",
+    },
+    {
+      id: 3,
+      name: "James",
+      imageUrl: "https://randomuser.me/api/portraits/men/20.jpg",
+    },
+    {
+      id: 4,
+      name: "Anna",
+      imageUrl: "https://randomuser.me/api/portraits/women/25.jpg",
+    },
+    {
+      id: 5,
+      name: "John",
+      imageUrl: "https://randomuser.me/api/portraits/men/22.jpg",
+    },
+  ];
+
+  const data = activeSection === "Baddies" ? baddiesData : vybersData;
+  const sectionTitle =
+    activeSection === "Baddies" ? "Baddie Story" : "Vybers Story";
+
   return (
     <SafeAreaView className="bg-white-normal h-full">
-      <ScrollView>
-        <View className="flex-row justify-center mt-4">
-          <TouchableOpacity className="px-4 py-2 rounded-full bg-gray-200">
-            <Text className="text-gray-900 font-semibold font-axiformaBlack">
-              Vybers
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="px-4 py-2 rounded-full bg-gradient-to-r from-[#9941EE] to-[#AA77FF] ml-4">
-            <Text className="text-white font-axiformaBlack">Baddie</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View className="bg-purple-normal mt-8 px-1">
-          <Text className="text-white-normal text-lg font-bold mt-2 ml-4 font-axiformaBlack">
-            Baddie Story
-          </Text>
-          <ScrollView
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            className="mt-4 ml-4"
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View className="flex-row items-center justify-center mt-4 mb-2">
+          <TouchableOpacity
+            className="w-[40%]"
+            onPress={() => setActiveSection("Vybers")}
           >
-            <TouchableOpacity className="rounded-full overflow-hidden w-14 h-14 border-2 border-dashed border-white-normal flex justify-center items-center bg-transparent mr-4">
-              <Text className="text-white-normal text-center font-axiformaRegular text-sm">
-                Add Story
-              </Text>
-            </TouchableOpacity>
-            {storiesData.map((story) => (
-              <View key={story.id} className="mr-4 mb-2">
-                <TouchableOpacity className="rounded-full overflow-hidden w-14 h-14 border-2 border-white-normal">
-                  <Image
-                    source={{ uri: story.imageUrl }}
-                    className="w-full h-full"
-                  />
-                </TouchableOpacity>
-                <Text className="text-white-normal text-center mt-2 font-axiformaRegular text-xs">
-                  {story.name}
+            {activeSection === "Vybers" ? (
+              <LinearGradient
+                colors={["#9C27B0", "#4CAF50"]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 0 }}
+                className="px-4 py-3 rounded-full"
+              >
+                <Text className="text-white-normal font-axiformaBlack text-center text-base">
+                  Vybers
+                </Text>
+              </LinearGradient>
+            ) : (
+              <View className="px-4 py-3 rounded-full border-[#D3E6E6] border">
+                <Text className="text-[#344559] font-axiformaBlack text-center text-base">
+                  Vybers
                 </Text>
               </View>
-            ))}
-          </ScrollView>
+            )}
+          </TouchableOpacity>
+          <TouchableOpacity
+            className="w-[40%] ml-4"
+            onPress={() => setActiveSection("Baddies")}
+          >
+            {activeSection === "Baddies" ? (
+              <LinearGradient
+                colors={["#9C27B0", "#4CAF50"]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 0 }}
+                className="px-4 py-3 rounded-full"
+              >
+                <Text className="text-white-normal font-axiformaBlack text-center text-base">
+                  Baddie
+                </Text>
+              </LinearGradient>
+            ) : (
+              <View className="px-4 py-3 rounded-full border-[#D3E6E6] border">
+                <Text className="text-[#344559] font-axiformaBlack text-center text-base">
+                  Baddie
+                </Text>
+              </View>
+            )}
+          </TouchableOpacity>
         </View>
 
-        <View className="bg-gray-800 overflow-hidden">
+        <ScrollView
+          className="bg-purple-normal px-6 py-4 mt-6"
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        >
+          <View>
+            <Text className="text-white-normal font-axiformaBlack text-lg mb-2">
+              {sectionTitle}
+            </Text>
+            <View className="flex-row items-center gap-4">
+              <View className="items-center">
+                <View className="border-2 border-white-normal border-dashed w-14 h-14 rounded-full mb-2 justify-center items-center">
+                  <Ionicons name="add-circle" size={30} color="#fff" />
+                </View>
+                <Text className="text-white-normal text-center font-axiformaRegular text-xs mt-2">
+                  Add Story
+                </Text>
+              </View>
+              {data.map((story) => (
+                <View key={story.id} className="items-center">
+                  <TouchableOpacity className="rounded-full overflow-hidden w-16 h-16 border-2 border-white-normal">
+                    <Image
+                      source={{ uri: story.imageUrl }}
+                      className="w-full h-full"
+                    />
+                  </TouchableOpacity>
+                  <Text className="text-white-normal text-center mt-2 font-axiformaRegular text-xs">
+                    {story.name}
+                  </Text>
+                </View>
+              ))}
+            </View>
+          </View>
+        </ScrollView>
+        <View>
           <Image
             source={{
-              uri: "https://images.unsplash.com/photo-1506126483163-f4d1558dbf85?q=80&w=2630&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+              uri:
+                activeSection === "Baddies"
+                  ? "https://images.unsplash.com/photo-1646133512747-babfd708d662?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  : "https://images.unsplash.com/photo-1627130595904-ebeeb6540a93?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             }}
-            className="w-full h-[400px] object-cover"
+            className="w-full h-[500px]"
+            resizeMode="cover"
           />
-          <View>
-            <View className="p-4">
-              <Text className="text-white-normal text-2xl font-axiformaBlack">
-                Dhemmex, 25
-              </Text>
-              <Text className="text-gray-300 mt-1 font-axiformaRegular">
-                Loves having fun and meeting new friends
-              </Text>
-              <View className="flex-row justify-between mt-4">
-                <View className="bg-gradient-to-r from-[#9941EE] to-[#AA77FF] px-3 py-1 rounded-full">
-                  <Text className="text-white">Baddie</Text>
-                </View>
-                <View className="bg-gray-700 px-3 py-1 rounded-full">
-                  <Text className="text-gray-300">2 Miles Away</Text>
-                </View>
-                <View className="bg-gray-700 px-3 py-1 rounded-full">
-                  <Text className="text-white">Available To Chat</Text>
+
+          <View className="absolute top-0 left-0 right-0 px-4 pt-4 flex-row justify-between">
+            {[...Array(5)].map((_, index) => (
+              <View
+                key={index}
+                className="w-[15%] h-[1px] bg-white-normal rounded-full"
+              />
+            ))}
+          </View>
+          <View className="flex-row items-center justify-between absolute top-[60%] w-full px-2">
+            <View className="bg-[#3d4c5ee9] py-8 px-4 rounded-[24px] border border-gray-500 w-[90%] mx-auto shadow-2xl">
+              <View className="flex-row gap-8 items-center mb-2">
+                <Image
+                  source={{
+                    uri:
+                      activeSection === "Baddies"
+                        ? "https://images.unsplash.com/photo-1646133512747-babfd708d662?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        : "https://images.unsplash.com/photo-1627130595904-ebeeb6540a93?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                  }}
+                  className="w-12 h-12 object-cover rounded-full border-2 border-red-300"
+                  resizeMode="cover"
+                />
+                <View className="flex-1">
+                  <View className="flex-row gap-2">
+                    <Text className="capitalize text-white-normal font-axiformaBlack text-xl">
+                      {activeSection === "Baddies" ? "Bolanle" : "Yvonne"}, 25
+                    </Text>
+                    <MaterialIcons name="verified" size={24} color="#6DD3E1" />
+                  </View>
+
+                  <Text className="capitalize font-axiformaRegular text-white-normal text-xs">
+                    Movie Addict and meeting great chef.
+                  </Text>
                 </View>
               </View>
+              <View className="flex-row flex-wrap gap-2">
+                <View className="border-2 border-[#86D8D3] rounded-full px-2 py-1 flex items-center justify-center">
+                  <Text className="text-[#65A29E] capitalize font-axiformaRegular text-xs">
+                    {activeSection === "Baddies" ? "Baddie" : "Vyber"}
+                  </Text>
+                </View>
+                <View className="border border-[#8BC0FE] rounded-full px-2 py-1 flex-row items-center justify-center">
+                  <Text className="text-[#6F9ACB] capitalize font-axiformaRegular text-xs ml-2">
+                    2 Miles Away
+                  </Text>
+                  <EvilIcons name="location" size={20} color="#6F9ACB" />
+                </View>
+                <View className="border-2 border-[#86D8D3] rounded-full px-2 py-1 flex items-center justify-center">
+                  <Text className="text-[#65A29E] capitalize font-axiformaRegular text-xs">
+                    Available
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <View className="flex-col gap-6 ml-1">
+              <TouchableOpacity>
+                <Ionicons name="chatbubbles-sharp" size={34} color="#fff" />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Feather name="user-plus" size={34} color="#fff" />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <MaterialIcons name="favorite" size={34} color="#fff" />
+              </TouchableOpacity>
             </View>
           </View>
         </View>
